774e6c7e52b8c102986c57b4bbccfaf8e56c3add
Andrey Koltsov <a99920641@gmail.com>
2018-11-07 23:36:21 +0300

    Delete unnecessary comments.

--- Chat/StorageCoreData.swift
+++ Chat/StorageCoreData.swift
@@ -25,24 +25,10 @@ class StorageCoreData: ProfileStorageManager {
     }
     
     static var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-         */
+
         let container = NSPersistentContainer(name: Constants.PROFILE_MODEL_TAG)
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 print(StorageCoreData.TAG, "An error has occured while loading the data:", error)
             }
         })
