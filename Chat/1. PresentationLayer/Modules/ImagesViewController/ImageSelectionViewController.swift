35a3098e0206a0aacf597d0e14d034944045489c
Andrey Koltsov <a99920641@gmail.com>
2018-12-08 13:49:04 +0300

    Add model and storyboard for collection view.

---
+++ Chat/1. PresentationLayer/Modules/ImagesViewController/ImageSelectionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ImageSelectionViewController.swift
+//  Chat
+//
+//  Created by Andrey Koltsov on 08/12/2018.
+//  Copyright © 2018 Peter the Great St.Petersburg Polytechnic University. All rights reserved.
+//
+
+import UIKit
+
+class ImageSelectionViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var imagesCollectionView: UICollectionView!
+    
+    // MARK: - Properties
+    private var imagesAmount: Int = 0 {
+        didSet {
+            imagesCollectionView.reloadData()
+        }
+    }
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
