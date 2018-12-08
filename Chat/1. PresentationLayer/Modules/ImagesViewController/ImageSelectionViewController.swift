9605eafcefa353d95c219dd83f248ec19539d1f3
Andrey Koltsov <a99920641@gmail.com>
2018-12-08 17:41:15 +0300

    Add setup for collection view controller.

--- Chat/1. PresentationLayer/Modules/ImagesViewController/ImageSelectionViewController.swift
+++ Chat/1. PresentationLayer/Modules/ImagesViewController/ImageSelectionViewController.swift
@@ -12,30 +12,54 @@ class ImageSelectionViewController: UIViewController {
 
     // MARK: - Outlets
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
     @IBOutlet weak var imagesCollectionView: UICollectionView!
     
     // MARK: - Properties
+    weak var delegate: ProfileImageDelegate?
+    private let IMAGE_REUSABLE_CELL_ID = "ImageCollectionViewCell"
+    private let IMAGE_NIB_NAME = "ImageCollectionViewCell"
+    
     private var imagesAmount: Int = 0 {
         didSet {
             imagesCollectionView.reloadData()
         }
     }
     
-    weak var delegate: ProfileImageDelegate?
-    
+    // MARK: - Dependencies
     private var imageDownloadService: IImageDownloadService?
+
 
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.configure()
     }
     
+
+    
     //MARK: - Public Methods
+    private func configure() {
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        
+        imagesCollectionView.register(UINib.init(nibName: self.IMAGE_NIB_NAME, bundle: nil), forCellWithReuseIdentifier: self.IMAGE_REUSABLE_CELL_ID)
+        
+        
+        
+        DispatchQueue.global().async { [weak self] in
+            self?.imageDownloadService?.performRequest { (itemsCount) in
+                DispatchQueue.main.async {
+                    self?.imagesAmount = itemsCount
+                    self?.activityIndicator.stopAnimating()
+                }
+            }
+        }
+    }
+    
     public func configureViewController(service: IImageDownloadService) {
         self.imageDownloadService = service
     }
+    
 }
