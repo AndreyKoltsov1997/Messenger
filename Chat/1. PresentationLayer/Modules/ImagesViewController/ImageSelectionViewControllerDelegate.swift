a2f1563115a1065b9047e0184eed6d293b8f34ee
Andrey Koltsov <a99920641@gmail.com>
2018-12-08 16:19:22 +0300

    Add ImageSelectionViewControllerDelegate.

---
+++ Chat/1. PresentationLayer/Modules/ImagesViewController/ImageSelectionViewControllerDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  ImageSelectionViewControllerDelegate.swift
+//  Chat
+//
+//  Created by Andrey Koltsov on 08/12/2018.
+//  Copyright © 2018 Peter the Great St.Petersburg Polytechnic University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol ImageSelectionViewControllerDelegate {
+    func setImage(image: UIImage?)
+}
