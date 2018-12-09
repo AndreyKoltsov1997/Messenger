d1ab3d513cc02ab2fc54bf60bc264e718177a9dc
Andrey Koltsov <a99920641@gmail.com>
2018-12-09 16:23:14 +0300

    Add tests target.

---
+++ NetworkTests/NetworkTests.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkTests.swift
+//  NetworkTests
+//
+//  Created by Andrey Koltsov on 09/12/2018.
+//  Copyright © 2018 Peter the Great St.Petersburg Polytechnic University. All rights reserved.
+//
+
+import XCTest
+@testable import Chat
+
+
+class NetworkTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+   
+
+}
